1d5555107d530080a38b2b2a4631923122004fce
Jamie Diprose <jdip004@aucklanduni.ac.nz>
2014-09-25 21:06:46 +1200

    Update run_blender_node.py

--- scripts/run_blender_node.py
+++ scripts/run_blender_node.py
@@ -9,4 +9,9 @@ armature_name = rospy.get_param("armature_name", "Armature")
 listener = BlenderTargetListener()
 joint_pub = BlenderJointStatePublisher()
 joint_pub.start()
-rospy.spin() #Have to spin otherwise the script quits
+
+show_gui = rospy.get_param("show_blender_gui", False)
+
+if not show_gui:
+    rospy.spin() #Have to spin otherwise the script quits
+    
